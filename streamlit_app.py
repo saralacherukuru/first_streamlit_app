f705de17b82fc20f3d4fc424db59fd33d8ff9999
Sarala Cherukuru <116864343+saralacherukuru@users.noreply.github.com>
2022-10-31 18:36:52 +0530

    Update streamlit_app.py
    adding secrets

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,8 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Output the screen as a table
 streamlit.dataframe(fruityvice_normalized)
 
-import snowflake.connector
+import streamlit.components.v1 as components
+from secrets import choice
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
